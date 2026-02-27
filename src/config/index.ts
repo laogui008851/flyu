24a6b28e46cbd75083e07e091f7322361459cd0d
Admin <admin@example.com>
2026-02-27 08:59:29 +0800

    Initial commit

---
+++ src/config/index.ts
@@ -0,0 +1,8 @@
+// ws 10001 api 10002 chat 10008 Admin 10009
+
+export const WS_URL = "wss://sp.junapi.xyz/msg_gateway";
+export const API_URL = 'https://sp.junapi.xyz/api';
+export const CHAT_URL = 'https://sp.junapi.xyz/chat';
+export const ACCOUNT_URL = 'https://sp.junapi.xyz/complete_admin';
+
+export const OBJECT_STORAGE = 'minio';
